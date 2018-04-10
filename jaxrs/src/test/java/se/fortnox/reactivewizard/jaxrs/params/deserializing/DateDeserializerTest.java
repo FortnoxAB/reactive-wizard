1c0ba901c3826b3265074908d360da0b01f96028
Niklas Gärdebrand <splitfeed@gmail.com>
2018-04-10 09:54:43 +0200

    Set time zone to prevent issues when tests are run in different timezones.

--- jaxrs/src/test/java/se/fortnox/reactivewizard/jaxrs/params/deserializing/DateDeserializerTest.java
+++ jaxrs/src/test/java/se/fortnox/reactivewizard/jaxrs/params/deserializing/DateDeserializerTest.java
@@ -1,26 +1,42 @@
 package se.fortnox.reactivewizard.jaxrs.params.deserializing;
 
 import com.fasterxml.jackson.databind.util.StdDateFormat;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import static org.fest.assertions.Assertions.assertThat;
 import static org.fest.assertions.Fail.fail;
 
 public class DateDeserializerTest {
     private final static Deserializer<Date> DESERIALIZER = new DateDeserializer(StdDateFormat::new);
+    private TimeZone previousTimeZone;
+
+    @Before
+    public void setup() {
+        previousTimeZone = TimeZone.getDefault();
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
+    @After
+    public void reset() {
+        TimeZone.setDefault(previousTimeZone);
+    }
 
     @Test
     public void shouldDeserialize() throws DeserializerException {
         Date deserialized = DESERIALIZER.deserialize("2010-01-01");
-        assertThat(deserialized.toString()).isEqualTo("Fri Jan 01 01:00:00 CET 2010");
+        assertThat(deserialized.toString()).isEqualTo("Fri Jan 01 00:00:00 UTC 2010");
     }
 
     @Test
     public void shouldDeserializeTimestampsInMilliseconds() throws DeserializerException {
         Date deserialized = DESERIALIZER.deserialize("1262304000000");
-        assertThat(deserialized.toString()).isEqualTo("Fri Jan 01 01:00:00 CET 2010");
+        assertThat(deserialized.toString()).isEqualTo("Fri Jan 01 00:00:00 UTC 2010");
     }
 
     @Test
