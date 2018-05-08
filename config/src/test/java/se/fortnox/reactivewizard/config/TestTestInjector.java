5c418cfa1acaef2b15977599a580f5c5e7317883
Jonas Fredin <jonas.fredin@fortnox.se>
2018-05-08 12:40:22 +0200

    Renaming test methods

--- config/src/test/java/se/fortnox/reactivewizard/config/TestTestInjector.java
+++ config/src/test/java/se/fortnox/reactivewizard/config/TestTestInjector.java
@@ -11,7 +11,7 @@ public class TestTestInjector {
 
 
     @Test
-    public void testWithOutConfigFile() {
+    public void testCreateWithOutConfigFile() {
         Injector injector = TestInjector.create();
         assertThat(injector.getInstance(ConfigAutoBindModule.class).getClass().getName()).isEqualToIgnoringCase("se.fortnox.reactivewizard.config.TestInjector$MockConfigAutoBindModule");
         assertThat(injector.getInstance(ConfigFactory.class).getClass().getName()).isNotEqualTo(ConfigFactory.class.getName());
@@ -22,7 +22,7 @@ public class TestTestInjector {
     }
 
     @Test
-    public void testConfigFile() {
+    public void testCreateWithConfigFile() {
         Injector injector = TestInjector.create("src/test/resources/testconfig.yml");
         assertThat(injector.getInstance(ConfigAutoBindModule.class).getClass().getName()).isEqualToIgnoringCase(ConfigAutoBindModule.class.getName());
         assertThat(injector.getInstance(ConfigFactory.class).getClass().getName()).isEqualTo(ConfigFactory.class.getName());
