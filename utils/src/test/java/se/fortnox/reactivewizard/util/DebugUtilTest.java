8c7325aa5981bb0400d412f276df64ca600c8169
Ivan Skachkov <ivan.sachkov@fortnox.se>
2018-08-29 18:41:36 +0200

    Consider also "maven.surefire.debug" environment variable while deciding if we are debugging
    * to make debugging a more seamless process if started from command line using `maven`.
    * trying to improve test coverage for "DebugUtil" by covering both branches for `isIdePresent` method.

--- utils/src/test/java/se/fortnox/reactivewizard/util/DebugUtilTest.java
+++ utils/src/test/java/se/fortnox/reactivewizard/util/DebugUtilTest.java
@@ -1,13 +1,40 @@
 package se.fortnox.reactivewizard.util;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
 
 public class DebugUtilTest {
 
     @Test
-    public void testIsIdePresent() {
-        assertThat(DebugUtil.IS_DEBUG).isNotNull();
+    public void testIsDebug() {
+        assertThat(DebugUtil.IS_DEBUG).isNotNull(); // will be true if running from Intellij IDEA and false from command line
+    }
+
+    @Test
+    public void testIsIdePresent() throws MalformedURLException {
+        ClassLoader classLoaderWithIdeaDebugger = Mockito.mock(ClassLoader.class);
+        when(classLoaderWithIdeaDebugger.getResource(eq("com/intellij")))
+                .thenReturn(new URL("jar:file:/path/to/debugger-agent-storage.jar!/com/intellij"));
+
+        assertThat(DebugUtil.isIdePresent(classLoaderWithIdeaDebugger)).isTrue();
+    }
+
+    @Test
+    public void testIsMavenDebug() {
+        String debugPropName = "maven.surefire.debug";
+        boolean originalSurefireDebugProp = Boolean.getBoolean(debugPropName);
+        System.setProperty(debugPropName, "true");
+
+        assertThat(DebugUtil.isMavenDebug()).isTrue();
+
+        // Clean up
+        System.setProperty(debugPropName, String.valueOf(originalSurefireDebugProp));
     }
 }
