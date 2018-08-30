f129ab84c8c9fd6009c7bd8f634005c2c52365ce
Ivan Skachkov <ivan.sachkov@fortnox.se>
2018-08-30 16:06:34 +0200

    Improve test coverage for "DebugUtil"

--- utils/src/test/java/se/fortnox/reactivewizard/util/DebugUtilTest.java
+++ utils/src/test/java/se/fortnox/reactivewizard/util/DebugUtilTest.java
@@ -7,7 +7,13 @@ import static org.fest.assertions.Assertions.assertThat;
 public class DebugUtilTest {
 
     @Test
-    public void testIsIdePresent() {
-        assertThat(DebugUtil.IS_DEBUG).isNotNull();
+    public void testIsDebug() {
+        String debugPropName = "maven.surefire.debug";
+        boolean originalSurefireDebugProp = Boolean.getBoolean(debugPropName);
+        System.setProperty(debugPropName, "true");
+
+        assertThat(DebugUtil.IS_DEBUG).isTrue();
+
+        System.setProperty(debugPropName, String.valueOf(originalSurefireDebugProp));
     }
 }
