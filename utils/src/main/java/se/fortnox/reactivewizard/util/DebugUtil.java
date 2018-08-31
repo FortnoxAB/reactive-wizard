0af5b0a9ce7695e139466528bee40a9111ebbc1c
Ivan Skachkov <ivan.sachkov@fortnox.se>
2018-08-31 14:51:27 +0200

    Trying to meet the coverage/complexity goals for the "DebugUtil" class (second attempt)
    * by adding covering both branches for `isIdePresent` method.

--- utils/src/main/java/se/fortnox/reactivewizard/util/DebugUtil.java
+++ utils/src/main/java/se/fortnox/reactivewizard/util/DebugUtil.java
@@ -4,17 +4,17 @@ package se.fortnox.reactivewizard.util;
  * Utilities for debugging in the development environment.
  */
 public class DebugUtil {
-    public static final boolean IS_DEBUG = isIdePresent() || isMavenDebug();
+    public static final boolean IS_DEBUG = isIdePresent(DebugUtil.class.getClassLoader()) || isMavenDebug();
 
     /**
      * Checks for the presence of an IDE, currently just IntelliJ.
+     * @param classLoader to use for the check
      */
-    private static boolean isIdePresent() {
-        ClassLoader classLoader = DebugUtil.class.getClassLoader();
+    static boolean isIdePresent(ClassLoader classLoader) {
         return classLoader.getResource("com/intellij") != null;
     }
 
-    private static boolean isMavenDebug() {
+    static boolean isMavenDebug() {
         return Boolean.getBoolean("maven.surefire.debug");
     }
 }
