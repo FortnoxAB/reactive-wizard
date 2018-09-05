c8629776f7dd1352eee0522062b553c3474c6cc0
Niklas Gärdebrand <splitfeed@gmail.com>
2018-09-05 12:11:50 +0200

    Improve code coverage of ExceptionHandler

--- jaxrs/src/test/java/se/fortnox/reactivewizard/jaxrs/ExceptionHandlerTest.java
+++ jaxrs/src/test/java/se/fortnox/reactivewizard/jaxrs/ExceptionHandlerTest.java
@@ -1,6 +1,7 @@
 package se.fortnox.reactivewizard.jaxrs;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.reactivex.netty.protocol.http.server.HttpServerRequest;
 import io.reactivex.netty.protocol.http.server.HttpServerResponse;
@@ -9,6 +10,8 @@ import org.apache.log4j.Appender;
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.junit.Test;
+import rx.exceptions.CompositeException;
+import rx.exceptions.OnErrorThrowable;
 import se.fortnox.reactivewizard.ExceptionHandler;
 import se.fortnox.reactivewizard.mocks.MockHttpServerResponse;
 
@@ -62,6 +65,65 @@ public class ExceptionHandlerTest {
             "ClosedChannelException: GET /path");
     }
 
+    @Test
+    public void shouldReturnLastExceptionOfCompositeException() {
+        WebException firstException = new WebException(HttpResponseStatus.INTERNAL_SERVER_ERROR).withLogLevel(org.slf4j.event.Level.DEBUG);
+        WebException secondException = new WebException(HttpResponseStatus.BAD_GATEWAY).withLogLevel(org.slf4j.event.Level.WARN);
+
+        CompositeException compositeException = new CompositeException(firstException, secondException);
+
+        String expectedLog = "502 Bad Gateway\n" +
+            "\tCause: -\n" +
+            "\tResponse: {\"id\":\"*\",\"error\":\"badgateway\"}\n" +
+            "\tRequest: GET /path headers: ";
+
+        assertLog(new MockHttpServerRequest("/path"), compositeException, Level.WARN, expectedLog);
+    }
+
+    @Test
+    public void shouldReturnCauseOfOnErrorThrowable() {
+        WebException cause = new WebException(HttpResponseStatus.BAD_GATEWAY).withLogLevel(org.slf4j.event.Level.WARN);
+
+        OnErrorThrowable onErrorThrowable = OnErrorThrowable.from(cause);
+
+        String expectedLog = "502 Bad Gateway\n" +
+            "\tCause: -\n" +
+            "\tResponse: {\"id\":\"*\",\"error\":\"badgateway\"}\n" +
+            "\tRequest: GET /path headers: ";
+
+        assertLog(new MockHttpServerRequest("/path"), onErrorThrowable, Level.WARN, expectedLog);
+    }
+
+    @Test
+    public void shouldSetLogLevelFromWebException() {
+        WebException cause = new WebException(HttpResponseStatus.BAD_GATEWAY);
+
+        cause.setLogLevel(org.slf4j.event.Level.WARN);
+        assertLog(new MockHttpServerRequest("/path"), cause, Level.WARN, "*");
+
+        cause.setLogLevel(org.slf4j.event.Level.DEBUG);
+        assertLog(new MockHttpServerRequest("/path"), cause, Level.DEBUG, "*");
+
+        cause.setLogLevel(org.slf4j.event.Level.ERROR);
+        assertLog(new MockHttpServerRequest("/path"), cause, Level.ERROR, "*");
+
+        cause.setLogLevel(org.slf4j.event.Level.TRACE);
+        assertLog(new MockHttpServerRequest("/path"), cause, Level.DEBUG, "*");
+
+        cause.setLogLevel(org.slf4j.event.Level.INFO);
+        assertLog(new MockHttpServerRequest("/path"), cause, Level.INFO, "*");
+    }
+
+    @Test
+    public void shouldSetContentLengthZeroFromHEAD() {
+        MockHttpServerRequest       request   = new MockHttpServerRequest("/path", HttpMethod.HEAD);
+        HttpServerResponse<ByteBuf> response  = new MockHttpServerResponse();
+        WebException                exception = new WebException(HttpResponseStatus.BAD_GATEWAY);
+        new ExceptionHandler().handleException(request, response, exception);
+
+        assertThat(response.getHeader("Content-Length")).isEqualTo("0");
+    }
+
     private void assertLog(HttpServerRequest<ByteBuf> request, Exception exception, Level expectedLevel, String expectedLog) {
         Appender mockAppender = mock(Appender.class);
         LogManager.getLogger(ExceptionHandler.class).addAppender(mockAppender);
@@ -78,7 +140,9 @@ public class ExceptionHandlerTest {
 
         verify(mockAppender).doAppend(matches(event -> {
             assertThat(event.getLevel()).isEqualTo(expectedLevel);
-            assertThat(event.getMessage().toString()).matches(regex);
+            if (!expectedLog.equals("*")) {
+                assertThat(event.getMessage().toString()).matches(regex);
+            }
         }));
     }
 }
