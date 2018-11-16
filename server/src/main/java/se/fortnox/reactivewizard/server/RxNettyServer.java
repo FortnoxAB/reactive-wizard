f2646c36097de73b3910b7b2a53523d0d2a99e06
Jonas Häll <jonas.hall@fortnox.se>
2018-11-16 10:44:11 +0100

    Register shutdown hook to process all connections before terminating the system

--- server/src/main/java/se/fortnox/reactivewizard/server/RxNettyServer.java
+++ server/src/main/java/se/fortnox/reactivewizard/server/RxNettyServer.java
@@ -2,9 +2,12 @@ package se.fortnox.reactivewizard.server;
 
 import io.netty.buffer.ByteBuf;
 import io.reactivex.netty.protocol.http.server.HttpServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Runs an RxNetty @{@link HttpServer} with all registered @{@link io.reactivex.netty.protocol.http.server.RequestHandler}s.
@@ -12,16 +15,19 @@ import javax.inject.Singleton;
 @Singleton
 public class RxNettyServer extends Thread {
 
+    private static final Logger logger = LoggerFactory.getLogger(RxNettyServer.class);
     private final ServerConfig config;
     private final CompositeRequestHandler compositeRequestHandler;
+    private final ConnectionCounter connectionCounter;
     public static final int MAX_CHUNK_SIZE_DEFAULT = 8192;
     private final HttpServer<ByteBuf, ByteBuf> server;
 
     @Inject
-    public RxNettyServer(ServerConfig config, CompositeRequestHandler compositeRequestHandler) {
+    public RxNettyServer(ServerConfig config, CompositeRequestHandler compositeRequestHandler, ConnectionCounter connectionCounter) {
         super("RxNettyServerMain");
         this.config = config;
         this.compositeRequestHandler = compositeRequestHandler;
+        this.connectionCounter = connectionCounter;
 
         if (config.isEnabled()) {
             server = HttpServer.newServer(config.getPort())
@@ -33,6 +39,7 @@ public class RxNettyServer extends Thread {
                     .start(compositeRequestHandler);
 
             start();
+            registerShutdownHook();
         } else {
             server = null;
         }
@@ -48,4 +55,17 @@ public class RxNettyServer extends Thread {
     public HttpServer<ByteBuf, ByteBuf> getServer() {
         return server;
     }
+
+    private void registerShutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Shutdown requested. Will wait up to 120 seconds...");
+            server.shutdown();
+            if (!connectionCounter.awaitZero(120, TimeUnit.SECONDS)) {
+                logger.error("Shutdown proceeded while connection count was not zero: " + connectionCounter.getCount());
+            }
+            server.awaitShutdown();
+            logger.info("Shutdown complete");
+        }));
+    }
+
 }
