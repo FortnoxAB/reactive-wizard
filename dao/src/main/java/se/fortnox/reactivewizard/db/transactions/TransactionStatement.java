8afaa972e288d6f345e7d384bd3b3dfb99a55062
jesper <jesper.lahdevirta@fortnox.se>
2018-11-26 11:55:29 +0100

    changed tabs to spaces

--- dao/src/main/java/se/fortnox/reactivewizard/db/transactions/TransactionStatement.java
+++ dao/src/main/java/se/fortnox/reactivewizard/db/transactions/TransactionStatement.java
@@ -8,54 +8,54 @@ import java.sql.SQLException;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class TransactionStatement implements Batchable {
-	private final AtomicReference<Statement> statement;
-	private final Transaction                transaction;
+    private final AtomicReference<Statement> statement;
+    private final Transaction                transaction;
 
-	public TransactionStatement(Transaction transaction) {
-		this.transaction = transaction;
-		this.statement = new AtomicReference<>();
-	}
+    public TransactionStatement(Transaction transaction) {
+        this.transaction = transaction;
+        this.statement = new AtomicReference<>();
+    }
 
-	public boolean isSubscribed() {
-		return statement.get() != null;
-	}
+    public boolean isSubscribed() {
+        return statement.get() != null;
+    }
 
-	public void executeStatement(Statement statement) {
-		if (this.statement.compareAndSet(null, statement)) {
-			transaction.subscribed();
-		} else {
-			throw new TransactionAlreadyExecutedException();
-		}
-	}
+    public void executeStatement(Statement statement) {
+        if (this.statement.compareAndSet(null, statement)) {
+            transaction.subscribed();
+        } else {
+            throw new TransactionAlreadyExecutedException();
+        }
+    }
 
-	public Statement getStatement() {
-		return statement.get();
-	}
+    public Statement getStatement() {
+        return statement.get();
+    }
 
-	public void removeStatement() {
-		statement.set(null);
-	}
+    public void removeStatement() {
+        statement.set(null);
+    }
 
-	public void setConnectionProvider(ConnectionProvider connectionProvider) {
-		transaction.setConnectionProvider(connectionProvider);
-	}
+    public void setConnectionProvider(ConnectionProvider connectionProvider) {
+        transaction.setConnectionProvider(connectionProvider);
+    }
 
-	public Transaction getTransaction() {
-		return transaction;
-	}
+    public Transaction getTransaction() {
+        return transaction;
+    }
 
-	public AtomicReference<Statement> getAtomicStatement() {
-		return statement;
-	}
+    public AtomicReference<Statement> getAtomicStatement() {
+        return statement;
+    }
 
-	@Override
-	public boolean sameBatch(Batchable batchable) {
-		return batchable instanceof TransactionStatement
-			&& getStatement().sameBatch(((TransactionStatement)batchable).getStatement());
-	}
+    @Override
+    public boolean sameBatch(Batchable batchable) {
+        return batchable instanceof TransactionStatement
+            && getStatement().sameBatch(((TransactionStatement)batchable).getStatement());
+    }
 
-	@Override
-	public void execute(Connection connection) throws SQLException {
-		getStatement().execute(connection);
-	}
+    @Override
+    public void execute(Connection connection) throws SQLException {
+        getStatement().execute(connection);
+    }
 }
