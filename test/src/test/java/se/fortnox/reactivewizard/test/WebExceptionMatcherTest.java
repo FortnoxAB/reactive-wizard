b6c294a48ab65090617e6f79529757c908327a6d
Claes Lööf <claes.loof@fortnox.se>
2019-10-04 14:56:23 +0200

    Added WebExceptionMatcher to cleanly assert WebExceptions

---
+++ test/src/test/java/se/fortnox/reactivewizard/test/WebExceptionMatcherTest.java
@@ -0,0 +1,59 @@
+package se.fortnox.reactivewizard.test;
+
+import io.netty.handler.codec.http.HttpResponseStatus;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import se.fortnox.reactivewizard.jaxrs.WebException;
+
+import static java.util.Optional.ofNullable;
+import static org.fest.assertions.Assertions.assertThat;
+
+public class WebExceptionMatcherTest {
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void shouldExpectCorrectErrorCode() {
+        String expectedErrorCode = "error.code";
+
+        expectedException.expect(WebException.class);
+        expectedException.expect(WebExceptionMatcher.has(expectedErrorCode));
+
+        throwWebException(null, expectedErrorCode);
+    }
+
+    @Test
+    public void shouldExpectCorrectStatus() {
+        HttpResponseStatus expectedStatus = HttpResponseStatus.INTERNAL_SERVER_ERROR;
+
+        expectedException.expect(WebException.class);
+        expectedException.expect(WebExceptionMatcher.has(expectedStatus));
+
+        throwWebException(expectedStatus, null);
+    }
+
+    @Test
+    public void shouldExpectCorrectStatusAndErrorCode() {
+        HttpResponseStatus expectedStatus    = HttpResponseStatus.INTERNAL_SERVER_ERROR;
+        String             expectedErrorCode = "error.code";
+
+        expectedException.expect(WebException.class);
+        expectedException.expect(WebExceptionMatcher.has(expectedStatus, expectedErrorCode));
+
+        throwWebException(expectedStatus, expectedErrorCode);
+    }
+
+    @Test
+    public void shouldNotExpectErrorAsDefault() {
+        int sum = 1 + 1;
+
+        assertThat(sum).isEqualTo(2);
+    }
+
+    private static void throwWebException(HttpResponseStatus expectedStatus, String expectedErrorCode) {
+        HttpResponseStatus status = ofNullable(expectedStatus).orElse(HttpResponseStatus.BAD_REQUEST);
+
+        throw new WebException(status, expectedErrorCode);
+    }
+}
