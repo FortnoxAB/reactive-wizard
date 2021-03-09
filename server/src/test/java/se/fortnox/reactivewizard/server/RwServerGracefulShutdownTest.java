1a1609eff5ce399803a55227d1f96be2024d8a1f
Jesper Holmström <jesper.holmstrom@fortnox.se>
2021-03-09 12:46:41 +0100

    Added five second wait before server shutdown for improved Kubernetes interoperability. Removed tests for graceful shutdown since they did not test what was intended. Removed LoopResources from RwServer since it was never used. Upped reactor-bom to SR17.

--- server/src/test/java/se/fortnox/reactivewizard/server/RwServerGracefulShutdownTest.java
+++ server/src/test/java/se/fortnox/reactivewizard/server/RwServerGracefulShutdownTest.java
@@ -4,89 +4,70 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import reactor.core.publisher.Mono;
+import reactor.netty.DisposableServer;
 import reactor.netty.http.client.HttpClient;
 import se.fortnox.reactivewizard.ExceptionHandler;
 import se.fortnox.reactivewizard.RequestHandler;
 
 import java.time.Duration;
 import java.util.Collections;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
-import static org.fest.assertions.Assertions.assertThat;
-import static org.junit.Assert.fail;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.awaitility.Awaitility.await;
 
 public class RwServerGracefulShutdownTest {
 
     private ServerConfig serverConfig;
+    private AtomicLong requests;
+    private AtomicLong responses;
 
     @Before
     public void before() {
         serverConfig = new ServerConfig();
         serverConfig.setPort(0);
+        requests = new AtomicLong(0);
+        responses = new AtomicLong(0);
     }
 
     @Test
-    public void shouldThrowExceptionIfGracefulShutDownIsNotPossible() throws InterruptedException {
-        CountDownLatch serverReceivedClientRequest = new CountDownLatch(1);
-        CountDownLatch clientReceivedResponse = new CountDownLatch(1);
+    public void shouldThrowExceptionIfGracefulShutDownIsNotPossible() {
         serverConfig.setShutdownTimeoutMs(1);
 
         // start server with a endpoint that takes 30 seconds to respond.
         Duration longResponseTime = Duration.ofSeconds(30);
-        RwServer rwServer = server(withEndpoint(serverReceivedClientRequest, longResponseTime));
+        RwServer rwServer = server(withEndpoint(longResponseTime));
 
         // client connects to endpoint and waits for server to respond.
-        clientSendsRequestToSlowEndpoint(rwServer,clientReceivedResponse);
+        clientSendsRequestToSlowEndpoint(rwServer);
 
-        assertThat(serverReceivedClientRequest.await(3, TimeUnit.SECONDS))
-            .describedAs("server should receive the client request")
-            .isTrue();
+        await()
+            .atMost(Duration.ofSeconds(3))
+            .until(() -> requests.get() == 1);
 
-        try {
-            // shutdown of server starts. A graceful shutdown is attempted.
-            RwServer.shutdownHook(serverConfig, rwServer.getServer(), null, new ConnectionCounter());
-            fail("Server should throw an exception that graceful shutdown could not be done. Expected IllegalStateException");
-        } catch (IllegalStateException illegalStateException) {
-                assertThat(illegalStateException.getMessage())
-                    .describedAs("server should throw an exception that graceful shutdown could not be done. The connection was cut.")
-                    .isEqualTo("Socket couldn't be stopped within 1000ms");
-        }
+        DisposableServer server = rwServer.getServer();
+        ConnectionCounter connectionCounter = new ConnectionCounter();
+
+        assertThatExceptionOfType(IllegalStateException.class)
+            .describedAs("server should throw an exception that graceful shutdown could not be done. The connection was cut.")
+            .isThrownBy(() -> RwServer.shutdownHook(serverConfig, server, connectionCounter))
+            .withMessage("Socket couldn't be stopped within 1000ms");
     }
 
     @Test
-    public void shouldGracefullyShutDown() throws InterruptedException {
-        CountDownLatch serverReceivedClientRequest = new CountDownLatch(1);
-        CountDownLatch clientReceivedResponse = new CountDownLatch(1);
-        serverConfig.setShutdownTimeoutMs(20);
-
-        // start server with a endpoint that takes 1 seconds to respond.
-        Duration responseTime = Duration.ofSeconds(1);
-        RwServer rwServer = server(withEndpoint(serverReceivedClientRequest, responseTime));
-
-        // client connects to endpoint and waits for server to respond.
-        clientSendsRequestToSlowEndpoint(rwServer,clientReceivedResponse);
-
-        assertThat(serverReceivedClientRequest.await(3, TimeUnit.SECONDS))
-            .describedAs("server should receive the client request")
-            .isTrue();
-
-        // shutdown of server starts. A graceful shutdown is attempted.
-        RwServer.shutdownHook(serverConfig, rwServer.getServer(), null, new ConnectionCounter());
-
-        // server waits for connection to complete before termination.
-        assertThat(clientReceivedResponse.await(5, TimeUnit.SECONDS))
-            .describedAs("client should get response")
-            .isTrue();
-
-        assertThat(rwServer.getServer().isDisposed())
-            .describedAs("server should shutdown")
-            .isTrue();
+    public void shouldWaitFiveSecondsBeforeDisposingServer() {
+        RwServer rwServer = server(withEndpoint(Duration.ofSeconds(1)));
+        Thread shutdown = new Thread(() -> RwServer.shutdownHook(serverConfig, rwServer.getServer(), new ConnectionCounter()));
+        shutdown.start();
+        await("Server should not be disposed until after five seconds")
+            .atLeast(5, SECONDS)
+            .until(() -> rwServer.getServer().isDisposed());
     }
 
-    private RequestHandler withEndpoint(CountDownLatch connectionReceived, Duration responseTime) {
+    private RequestHandler withEndpoint(Duration responseTime) {
         return (httpServerRequest, httpServerResponse) -> {
-            connectionReceived.countDown();
+            requests.incrementAndGet();
             return httpServerResponse
                 .chunkedTransfer(false)
                 .sendByteArray(Mono.just("Message that we want to receive before server terminates".getBytes())
@@ -94,7 +75,7 @@ public class RwServerGracefulShutdownTest {
         };
     }
 
-    private void clientSendsRequestToSlowEndpoint(RwServer rwServer, CountDownLatch responseReceived) {
+    private void clientSendsRequestToSlowEndpoint(RwServer rwServer) {
         HttpClient.ResponseReceiver<?> responseReceiver = HttpClient.create()
             .baseUrl("http://localhost:" + rwServer.getServer().port())
             .get();
@@ -103,14 +84,13 @@ public class RwServerGracefulShutdownTest {
             .aggregate()
             .asString()
             .doOnNext(e -> {
-                responseReceived.countDown();
+                responses.incrementAndGet();
             }).subscribe();
     }
 
-
     private RwServer server(RequestHandler handler) {
-        ConnectionCounter       connectionCounter = new ConnectionCounter();
-        CompositeRequestHandler handlers          = new CompositeRequestHandler(Collections.singleton(handler), new ExceptionHandler(new ObjectMapper()), connectionCounter);
+        ConnectionCounter connectionCounter = new ConnectionCounter();
+        CompositeRequestHandler handlers = new CompositeRequestHandler(Collections.singleton(handler), new ExceptionHandler(new ObjectMapper()), connectionCounter);
         return new RwServer(serverConfig, handlers, connectionCounter) {
             @Override
             void registerShutdownHook() {
