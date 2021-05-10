9207f11a5ac7674fa727996434cf93185d8e5f96
Rickard Blomkvist <rickard.blomkvist@fortnox.se>
2021-05-10 08:37:58 +0200

    Restored imports

--- test/src/test/java/se/fortnox/reactivewizard/test/TestUtilTest.java
+++ test/src/test/java/se/fortnox/reactivewizard/test/TestUtilTest.java
@@ -10,7 +10,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static se.fortnox.reactivewizard.test.TestUtil.*;
+import static se.fortnox.reactivewizard.test.TestUtil.assertException;
+import static se.fortnox.reactivewizard.test.TestUtil.assertNestedException;
+import static se.fortnox.reactivewizard.test.TestUtil.matches;
 
 public class TestUtilTest {
     @Test
