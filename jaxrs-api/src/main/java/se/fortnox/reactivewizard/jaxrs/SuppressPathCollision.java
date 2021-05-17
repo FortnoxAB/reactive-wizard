0021d56999b02d0861d65b9a05f00522dc008f62
Jesper Holmström <jesper.holmstrom@fortnox.se>
2021-05-17 08:58:33 +0200

    Move SuppressPathCollision annotation to the jaxrs-api module.

--- jaxrs-api/src/main/java/se/fortnox/reactivewizard/jaxrs/SuppressPathCollision.java
+++ jaxrs-api/src/main/java/se/fortnox/reactivewizard/jaxrs/SuppressPathCollision.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotate a resource method with this to exclude it from the colliding paths startup check.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface SuppressPathCollision {
