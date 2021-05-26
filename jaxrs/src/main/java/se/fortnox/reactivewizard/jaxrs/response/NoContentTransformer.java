0fee32296614a1d6361e6926adfe2b0ef845379e
Tommy Persson <tommy.persson@fortnox.se>
2021-05-26 15:19:46 +0200

    Only rewrite statuses to 204 (No Content) when the original status was in the 2xx-range

--- jaxrs/src/main/java/se/fortnox/reactivewizard/jaxrs/response/NoContentTransformer.java
+++ jaxrs/src/main/java/se/fortnox/reactivewizard/jaxrs/response/NoContentTransformer.java
@@ -1,19 +1,22 @@
 package se.fortnox.reactivewizard.jaxrs.response;
 
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpStatusClass;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import se.fortnox.reactivewizard.jaxrs.JaxRsResource;
 
 /**
- * Sets the response status to 204 NO CONTENT if the returned Observable is empty
+ * Sets the response status to 204 NO CONTENT for successful responses if the returned Observable is empty
  */
 public class NoContentTransformer implements ResultTransformerFactory {
     @Override
     public <T> ResultTransformer<T> create(JaxRsResource<T> resource) {
         return (result, args) -> result.map(output -> {
             return output.switchIfEmpty(Flux.defer(() -> {
-                result.responseStatus = HttpResponseStatus.NO_CONTENT;
+                if (result.responseStatus.codeClass() == HttpStatusClass.SUCCESS) {
+                    result.responseStatus = HttpResponseStatus.NO_CONTENT;
+                }
+
                 return Flux.empty();
             }));
         });
