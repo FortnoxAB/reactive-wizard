d452fcf4bf3d3867fdd3561d43d0d674156178f1
Jesper Holmström <jesper.holmstrom@fortnox.se>
2021-06-14 09:36:04 +0200

    Fixed checkstyle error

--- jaxrs-api/src/main/java/se/fortnox/reactivewizard/jaxrs/SuppressMissingPathParam.java
+++ jaxrs-api/src/main/java/se/fortnox/reactivewizard/jaxrs/SuppressMissingPathParam.java
@@ -7,7 +7,8 @@ import java.lang.annotation.Target;
 
 /**
  * Annotate a resource method/interface with this annotation and list what paramName(s) you want to exclude from the missing path param startup check.
- * <p>To ignore missing path param errors on a resource with path <i>/api/{name}</i> simply add <i>@SuppressMissingPathParam(paramNames = "name")</i> to the resource method.</p>
+ * <p>To ignore missing path param errors on a resource with path <i>/api/{name}</i> simply add
+ * <i>@SuppressMissingPathParam(paramNames = "name")</i> to the resource method.</p>
  * <p><b>Note:</b> paramNames are case sensitive.</p>
  */
 @Retention(RetentionPolicy.RUNTIME)
