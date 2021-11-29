d346dd60bd8816d50f39fb1148b2c5b3ba05ce66
Jonas Fredin <jonas.fredin@fortnox.se>
2021-11-29 16:30:54 +0100

    Add the @VisibleForTesting annotation Trying to clarify the error message.

--- bootstrap/src/main/java/se/fortnox/reactivewizard/SetupModule.java
+++ bootstrap/src/main/java/se/fortnox/reactivewizard/SetupModule.java
@@ -1,5 +1,6 @@
 package se.fortnox.reactivewizard;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.inject.Binder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,9 +26,11 @@ public class SetupModule implements AutoBindModule {
      * @param logger the logger to be used
      * @param throwable the exception
      */
+    @VisibleForTesting
     void warnLogException(Logger logger, Throwable throwable) {
-        logger.warn("Tried to send item or error to subscriber but the subscriber had already left. " +
-            "This could happen when you merge two (or more) observables and one reports an error while the other a moment later tries to " +
-            "call onError on the subscriber but the subscriber already left at the first error.", throwable);
+        logger.warn("Tried to send an onError signal to the subscriber but the subscriber had already left" +
+            "This can happen when a publisher calls onError on the operator despite having already called onError on it previously and the spec says " +
+            "never should a subscriber get two calls to onError. A typical example when this could happen is when you merge two observables and both " +
+            "of them reports an error", throwable);
     }
 }
