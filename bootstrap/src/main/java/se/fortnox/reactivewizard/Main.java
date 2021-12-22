7f4aee166aa6dd4a91509af211f8a0c419cb1eac
Jonas Fredin <jonas.fredin@fortnox.se>
2021-12-22 15:59:59 +0100

    wip log4j2 configuration files

--- bootstrap/src/main/java/se/fortnox/reactivewizard/Main.java
+++ bootstrap/src/main/java/se/fortnox/reactivewizard/Main.java
@@ -26,8 +26,6 @@ public class Main {
     public static void main(String[] args) {
         try {
             ConfigFactory  configFactory  = createConfigFactory(args);
-            LoggingFactory loggingFactory = configFactory.get(LoggingFactory.class);
-            loggingFactory.init();
             Module bootstrap = new AbstractModule() {
                 @Override
                 protected void configure() {
--- logging/src/main/java/se/fortnox/reactivewizard/logging/LoggingFactory.java
+++ logging/src/main/java/se/fortnox/reactivewizard/logging/LoggingFactory.java
@@ -1,156 +1,223 @@
 package se.fortnox.reactivewizard.logging;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Splitter;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.core.Filter;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.logging.log4j.core.config.builder.api.AppenderComponentBuilder;
-import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilder;
-import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilderFactory;
-import org.apache.logging.log4j.core.config.builder.api.FilterComponentBuilder;
-import org.apache.logging.log4j.core.config.builder.api.LayoutComponentBuilder;
-import org.apache.logging.log4j.core.config.builder.api.LoggerComponentBuilder;
-import org.apache.logging.log4j.core.config.builder.api.RootLoggerComponentBuilder;
-import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
-import se.fortnox.reactivewizard.config.Config;
-
-import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-
 /**
  * Factory for initializing logging configuration and also container of logging configuration from YAML.
  */
-@Config("logging")
 public class LoggingFactory {
 
-    @Valid
-    @JsonProperty("appenders")
-    Map<String, Map<String, String>> appenders;
-
-    @Valid
-    @JsonProperty("additivity")
-    Map<String, Boolean> additivity;
-
-    @Valid
-    @JsonProperty("level")
-    String level = "INFO";
-
-    @Valid
-    @JsonProperty("levels")
-    Map<String, String> levels = new HashMap<>();
-
-    private static final Map<String, String> TYPE_FROM_NAME = Map.of(
-        "stdout", "Console",
-        "file", "RollingFile");
-
-    /**
-     * Configures logging.
-     */
+    //@Valid
+    //@JsonProperty("appenders")
+    //Map<String, Map<String, String>> appenders = new HashMap<>();
+    //
+    //@Valid
+    //@JsonProperty("additivity")
+    //Map<String, Boolean> additivity;
+    //
+    //@Valid
+    //@JsonProperty("level")
+    //String level = "INFO";
+    //
+    //@Valid
+    //@JsonProperty("levels")
+    //Map<String, String> levels = new HashMap<>();
+    //
+    //private static final Map<String, String> TYPE_FROM_NAME = Map.of(
+    //    "stdout", "Console",
+    //    "file", "RollingFile");
+    //
+    ///**
+    // * Configures logging.
+    // */
     public void init() {
-        initLogging();
+        //initLogging();
     }
+    //
+    //@VisibleForTesting
+    //ConfigurationBuilder<BuiltConfiguration> initLogging() {
+    //
+    //    //Initialize configuration from config.yml
+    //    ConfigurationBuilder<BuiltConfiguration> builder = ConfigurationBuilderFactory.newConfigurationBuilder();
+    //    createAppenders(builder);
+    //    createLoggers(builder);
+    //    final BuiltConfiguration build = builder.build();
+    //
+    //    //Initialize logging from file
+    //    final LoggerContext ctx    = (LoggerContext) LogManager.getContext(false);
+    //    final Configuration config = ctx.getConfiguration();
+    //
+    //    //Set root logger level
+    //    config.getRootLogger().setLevel(Level.toLevel(level));
+    //
+    //    //Overwrite appenders if any
+    //    build.getAppenders().forEach((name, appender) -> {
+    //        if (config.getAppender(name) != null) {
+    //            final RenamedAppender renamedAppender = new RenamedAppender(appender);
+    //            config.addAppender(renamedAppender);
+    //
+    //            if (config.getRootLogger().getAppenders().containsKey(name)) {
+    //                config.getRootLogger().removeAppender(name);
+    //            }
+    //            config.getRootLogger().addAppender(renamedAppender, Level.toLevel(level), null);
+    //        }
+    //        else {
+    //            config.addAppender(appender);
+    //        }
+    //    });
+    //
+    //    //Overwrite loggers
+    //    build.getLoggers().forEach((name, loggerConfig) -> {
+    //
+    //        if (config.getLoggers().get(name) != null) {
+    //            config.getLoggers().get(name).setLevel(loggerConfig.getLevel());
+    //            config.getLoggers().get(name).setAdditive(loggerConfig.isAdditive());
+    //        } else {
+    //            config.addLogger(name, loggerConfig);
+    //        }
+    //    });
+    //
+    //    ctx.updateLoggers();
+    //
+    //    return builder;
+    //}
+    //
+    //private void createLoggers(ConfigurationBuilder<BuiltConfiguration> builder) {
+    //    levels.forEach((loggerName, levelAndAppenderString) -> {
+    //        String[] parts = levelAndAppenderString.split(",");
+    //        String levelPart = parts[0].trim();
+    //
+    //        LoggerComponentBuilder logger = builder.newAsyncLogger(loggerName, Level.toLevel(levelPart));
+    //        logger.addAttribute("additivity", additivity.getOrDefault(loggerName, false));
+    //
+    //        if (parts.length > 1) {
+    //            String appender = parts[1].trim();
+    //            logger.add(builder.newAppenderRef(appender));
+    //        }
+    //
+    //        builder.add(logger);
+    //    });
+    //}
+    //
+    //private void createAppenders(ConfigurationBuilder<BuiltConfiguration> builder) {
+    //    appenders.forEach((name, appenderProps) -> {
+    //        AppenderComponentBuilder appender = builder.newAppender(name, TYPE_FROM_NAME.get(name));
+    //        setAppenderAttributes(builder, appenderProps, appender);
+    //        builder.add(appender);
+    //    });
+    //}
+    //
+    //private void setAppenderAttributes(ConfigurationBuilder<BuiltConfiguration> builder, Map<String, String> appenderProps,
+    // AppenderComponentBuilder appender) {
+    //    appenderProps.forEach((key, value) -> {
+    //        if ("threshold".equals(key)) {
+    //            FilterComponentBuilder thresholdFilter = builder.newFilter("ThresholdFilter", Filter.Result.ACCEPT, Filter.Result.DENY);
+    //            thresholdFilter.addAttribute("level", value);
+    //            appender.add(thresholdFilter);
+    //        } else if ("layout".equals(key)) {
+    //            LayoutComponentBuilder layoutBuilder = builder.newLayout(value);
+    //            addLayoutProperties(appenderProps, layoutBuilder);
+    //            appender.add(layoutBuilder);
+    //        } else if ("pattern".equals(key)) {
+    //            appender.add(builder.newLayout("PatternLayout").addAttribute("pattern", value));
+    //        }  else if (!"layoutProperties".equals(key)) {
+    //            appender.addAttribute(key, value);
+    //        }
+    //    });
+    //}
+    //
+    //void addLayoutProperties(Map<String, String> appenderProps, LayoutComponentBuilder layoutBuilder) {
+    //    Splitter.on(" ")
+    //        .omitEmptyStrings()
+    //        .splitToList(appenderProps.getOrDefault("layoutProperties", ""))
+    //        .forEach((layoutProperty) -> {
+    //            List<String> propertyKeyValue = Splitter.on("=").splitToList(layoutProperty);
+    //            if (propertyKeyValue.size() != 2) {
+    //                throw new IllegalArgumentException("Bad formatted layout properties " + layoutProperty);
+    //            }
+    //            layoutBuilder.addAttribute(propertyKeyValue.get(0), propertyKeyValue.get(1));
+    //        });
+    //}
+    //
+    ///**
+    // * @return log level of the root logger.
+    // */
 
-    @VisibleForTesting
-    ConfigurationBuilder<BuiltConfiguration> initLogging() {
-        setDefaults();
-        ConfigurationBuilder<BuiltConfiguration> builder = ConfigurationBuilderFactory.newConfigurationBuilder();
-        createAppenders(builder);
-        createRootLogger(builder);
-        createLoggers(builder);
-        Configurator.initialize(builder.build());
-        return builder;
-    }
-
-    private void createLoggers(ConfigurationBuilder<BuiltConfiguration> builder) {
-        levels.forEach((loggerName, levelAndAppenderString) -> {
-            String[] parts = levelAndAppenderString.split(",");
-            String levelPart = parts[0].trim();
-
-            LoggerComponentBuilder logger = builder.newAsyncLogger(loggerName, Level.toLevel(levelPart));
-            logger.addAttribute("additivity", additivity.getOrDefault(loggerName, false));
-
-            if (parts.length > 1) {
-                String appender = parts[1].trim();
-                logger.add(builder.newAppenderRef(appender));
-            }
-
-            builder.add(logger);
-        });
-    }
-
-    private void createRootLogger(ConfigurationBuilder<BuiltConfiguration> builder) {
-        RootLoggerComponentBuilder rootLogger = builder.newAsyncRootLogger(Level.toLevel(level));
-        appenders.keySet().forEach(appenderName -> rootLogger.add(builder.newAppenderRef(appenderName)));
-        builder.add(rootLogger);
-    }
-
-    private void createAppenders(ConfigurationBuilder<BuiltConfiguration> builder) {
-        appenders.forEach((name, appenderProps) -> {
-            AppenderComponentBuilder appender = builder.newAppender(name, TYPE_FROM_NAME.get(name));
-            setAppenderAttributes(builder, appenderProps, appender);
-            builder.add(appender);
-        });
-    }
-
-    private void setAppenderAttributes(ConfigurationBuilder<BuiltConfiguration> builder, Map<String, String> appenderProps, AppenderComponentBuilder appender) {
-        appenderProps.forEach((key, value) -> {
-            if ("threshold".equals(key)) {
-                FilterComponentBuilder thresholdFilter = builder.newFilter("ThresholdFilter", Filter.Result.ACCEPT, Filter.Result.DENY);
-                thresholdFilter.addAttribute("level", value);
-                appender.add(thresholdFilter);
-            } else if ("layout".equals(key)) {
-                LayoutComponentBuilder layoutBuilder = builder.newLayout(value);
-                addLayoutProperties(appenderProps, layoutBuilder);
-                appender.add(layoutBuilder);
-            } else if ("pattern".equals(key)) {
-                appender.add(builder.newLayout("PatternLayout").addAttribute("pattern", value));
-            }  else if (!"layoutProperties".equals(key)) {
-                appender.addAttribute(key, value);
-            }
-        });
-    }
-
-    private void setDefaults() {
-        if (additivity == null) {
-            additivity = new HashMap<>();
-        }
-        if (appenders == null) {
-            appenders = new HashMap<>();
-            Map<String, String> stdoutAttributes = new HashMap<>();
-            appenders.put("stdout", stdoutAttributes);
-            stdoutAttributes.put("pattern", "%-5p [%d{yyyy-MM-dd HH:mm:ss.SSS}] %c: %m%n");
-        }
-    }
-
-    void addLayoutProperties(Map<String, String> appenderProps, LayoutComponentBuilder layoutBuilder) {
-        Splitter.on(" ")
-            .omitEmptyStrings()
-            .splitToList(appenderProps.getOrDefault("layoutProperties", ""))
-            .forEach((layoutProperty) -> {
-                List<String> propertyKeyValue = Splitter.on("=").splitToList(layoutProperty);
-                if (propertyKeyValue.size() != 2) {
-                    throw new IllegalArgumentException("Bad formatted layout properties " + layoutProperty);
-                }
-                layoutBuilder.addAttribute(propertyKeyValue.get(0), propertyKeyValue.get(1));
-            });
-    }
-
-    /**
-     * @return log level of the root logger.
-     */
     public String getLevel() {
-        return level;
+        return "info";
     }
 
-    /**
-     * @param level to set on the root logger.
-     */
+    //
+    ///**
+    // * @param level to set on the root logger.
+    // */
+
     public void setLevel(String level) {
-        this.level = level;
     }
+    //
+    //private static class RenamedAppender implements Appender {
+    //    private final Appender appender;
+    //
+    //    public RenamedAppender(Appender appender) {
+    //        this.appender = appender;
+    //    }
+    //
+    //    @Override
+    //    public void append(LogEvent event) {
+    //        appender.append(event);
+    //    }
+    //
+    //    @Override
+    //    public String getName() {
+    //        return appender.getName() + "rw";
+    //    }
+    //
+    //    @Override
+    //    public Layout<? extends Serializable> getLayout() {
+    //        return appender.getLayout();
+    //    }
+    //
+    //    @Override
+    //    public boolean ignoreExceptions() {
+    //        return appender.ignoreExceptions();
+    //    }
+    //
+    //    @Override
+    //    public ErrorHandler getHandler() {
+    //        return appender.getHandler();
+    //    }
+    //
+    //    @Override
+    //    public void setHandler(ErrorHandler handler) {
+    //        appender.setHandler(handler);
+    //    }
+    //
+    //    @Override
+    //    public State getState() {
+    //        return appender.getState();
+    //    }
+    //
+    //    @Override
+    //    public void initialize() {
+    //        appender.initialize();
+    //    }
+    //
+    //    @Override
+    //    public void start() {
+    //        appender.start();
+    //    }
+    //
+    //    @Override
+    //    public void stop() {
+    //        appender.stop();
+    //    }
+    //
+    //    @Override
+    //    public boolean isStarted() {
+    //        return appender.isStarted();
+    //    }
+    //
+    //    @Override
+    //    public boolean isStopped() {
+    //        return appender.isStopped();
+    //    }
+    //}
 }
