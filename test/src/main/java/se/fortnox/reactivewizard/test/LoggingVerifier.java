ec740c09a6df1ce5ec574c6eb22b32743bf3b65a
Jonas Fredin <jonas.fredin@fortnox.se>
2022-01-11 09:15:31 +0100

    fixed sonar warnings

--- test/src/main/java/se/fortnox/reactivewizard/test/LoggingVerifier.java
+++ test/src/main/java/se/fortnox/reactivewizard/test/LoggingVerifier.java
@@ -38,11 +38,15 @@ public class LoggingVerifier extends ExternalResource {
      * @param errorMessage the error message to assert
      */
     public void verify(Level level, String errorMessage) {
-        this.verify(level, logEvent -> {
-            assertThat(logEvent.getMessage().getFormattedMessage()).isEqualTo(errorMessage);
-        });
+        this.verify(level, logEvent -> assertThat(logEvent.getMessage().getFormattedMessage()).isEqualTo(errorMessage));
     }
 
+    /**
+     * Verify logging is carried out at a certain level.
+     * The logEventAsserter is responsible for asserting the correctness of the logEvent object
+     * @param level the log level
+     * @param logEventAsserter the log event asserter
+     */
     public void verify(Level level, Consumer<LogEvent> logEventAsserter) {
         Mockito.verify(appender, Mockito.atLeastOnce()).append(matches(logEvent -> {
             if (level.equals(logEvent.getLevel())) {
