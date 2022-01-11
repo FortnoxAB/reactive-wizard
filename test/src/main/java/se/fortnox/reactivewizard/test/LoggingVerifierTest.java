0c6106ef74ce2e6656f10a1deea00d197627b541
Jonas Fredin <jonas.fredin@fortnox.se>
2022-01-11 09:05:35 +0100

    fixed sonar warnings

--- test/src/main/java/se/fortnox/reactivewizard/test/LoggingVerifierTest.java
+++ test/src/main/java/se/fortnox/reactivewizard/test/LoggingVerifierTest.java
@@ -1,7 +1,6 @@
 package se.fortnox.reactivewizard.test;
 
 import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
 import org.junit.Rule;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -10,20 +9,18 @@ import org.slf4j.LoggerFactory;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LoggingVerifierTest {
-
-    //Unused it seems but is needed for the mocked appender to be set up properly
-    private static Logger LOG = LoggerFactory.getLogger(LoggingVerifierTest.class);
+    private static final Logger LOG = LoggerFactory.getLogger(LoggingVerifierTest.class);
 
     @Rule
     public LoggingVerifier loggingVerifier = new LoggingVerifier(LoggingVerifierTest.class);
 
     @Test
     public void verifyLogging() {
-        LogManager.getLogger(LoggingVerifierTest.class).trace("trace");
-        LogManager.getLogger(LoggingVerifierTest.class).debug("debug");
-        LogManager.getLogger(LoggingVerifierTest.class).error("error");
-        LogManager.getLogger(LoggingVerifierTest.class).warn("warn");
-        LogManager.getLogger(LoggingVerifierTest.class).info("info");
+        LOG.trace("trace");
+        LOG.debug("debug");
+        LOG.error("error");
+        LOG.warn("warn");
+        LOG.info("info");
 
         loggingVerifier.verify(Level.TRACE, "trace");
         loggingVerifier.verify(Level.DEBUG, "debug");
