fb746deea82a2dd2aac62c4f997619ca1b0a0163
Jonas Fredin <jonas.fredin@fortnox.se>
2022-04-07 11:03:37 +0200

    added real streaming tests

--- jaxrs/src/test/java/se/fortnox/reactivewizard/jaxrs/StreamingDataTest.java
+++ jaxrs/src/test/java/se/fortnox/reactivewizard/jaxrs/StreamingDataTest.java
@@ -1,15 +1,21 @@
 package se.fortnox.reactivewizard.jaxrs;
 
 import org.junit.Test;
+import reactor.core.publisher.Flux;
 import reactor.netty.DisposableServer;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.http.client.HttpClientResponse;
 import rx.Observable;
+import se.fortnox.reactivewizard.CollectionOptions;
+import se.fortnox.reactivewizard.db.MockDb;
+import se.fortnox.reactivewizard.db.Query;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
@@ -17,10 +23,11 @@ import java.util.concurrent.atomic.AtomicReference;
 import static org.assertj.core.api.Assertions.assertThat;
 import static rx.Observable.just;
 import static se.fortnox.reactivewizard.jaxrs.response.ResponseDecorator.withHeaders;
-import static se.fortnox.reactivewizard.utils.JaxRsTestUtil.*;
+import static se.fortnox.reactivewizard.utils.JaxRsTestUtil.testServer;
 
 public class StreamingDataTest {
-    private StreamingResource   streamingResource   = new StreamingResourceImpl();
+
+    private StreamingResource   streamingResource = new StreamingResourceImpl(null, null);
     private NoStreamingResource noStreamingResource = new NoStreamingResourceImpl();
 
     @Test
@@ -77,6 +84,103 @@ public class StreamingDataTest {
         }
     }
 
+    @Test
+    public void shouldNotSignalLastRecordIfMoreItems() throws SQLException {
+
+        MockDb mockDb = new MockDb();
+        StreamingDao streamingDao = mockDb.mockDao(10, StreamingDao.class);
+
+        CollectionOptions collectionOptions = new CollectionOptions();
+        StreamingResource streamingResource = new StreamingResourceImpl(streamingDao, collectionOptions);
+        DisposableServer server   = testServer(streamingResource).getServer();
+        try {
+            HttpClient client = HttpClient.create().port(server.port());
+            final AtomicReference<HttpClientResponse> response = new AtomicReference<>();
+            List<String> strings = client.get().uri("/stream/paging?limit=5").response((resp, body)->{
+                response.set(resp);
+                return body.asString();
+            }).collectList().block();
+
+            assertThat(strings).hasSize(5);
+            assertThat(collectionOptions.isLastRecord()).isFalse();
+        } finally {
+            server.disposeNow();
+        }
+    }
+
+
+    @Test
+    public void shouldSignalLastRecordIfNoMoreItems() throws SQLException {
+
+        MockDb mockDb = new MockDb();
+        StreamingDao streamingDao = mockDb.mockDao(10, StreamingDao.class);
+
+        CollectionOptions collectionOptions = new CollectionOptions();
+        StreamingResource streamingResource = new StreamingResourceImpl(streamingDao, collectionOptions);
+        DisposableServer server   = testServer(streamingResource).getServer();
+        try {
+            HttpClient client = HttpClient.create().port(server.port());
+            final AtomicReference<HttpClientResponse> response = new AtomicReference<>();
+            List<String> strings = client.get().uri("/stream/paging?limit=10").response((resp, body)->{
+                response.set(resp);
+                return body.asString();
+            }).collectList().block();
+
+            assertThat(strings).hasSize(10);
+            assertThat(collectionOptions.isLastRecord()).isTrue();
+        } finally {
+            server.disposeNow();
+        }
+    }
+
+    @Test
+    public void shouldSignalLastRecordIfLimitIsHigherThanItemCount() throws SQLException {
+
+        MockDb mockDb = new MockDb();
+        StreamingDao streamingDao = mockDb.mockDao(10, StreamingDao.class);
+
+        CollectionOptions collectionOptions = new CollectionOptions();
+        StreamingResource streamingResource = new StreamingResourceImpl(streamingDao, collectionOptions);
+        DisposableServer server   = testServer(streamingResource).getServer();
+        try {
+            HttpClient client = HttpClient.create().port(server.port());
+            final AtomicReference<HttpClientResponse> response = new AtomicReference<>();
+            List<String> strings = client.get().uri("/stream/paging?limit=15").response((resp, body)->{
+                response.set(resp);
+                return body.asString();
+            }).collectList().block();
+
+            assertThat(strings).hasSize(10);
+            assertThat(collectionOptions.isLastRecord()).isTrue();
+        } finally {
+            server.disposeNow();
+        }
+    }
+
+    @Test
+    public void shouldSignalLastRecordIfEmptyResponse() throws SQLException {
+
+        MockDb mockDb = new MockDb();
+        StreamingDao streamingDao = mockDb.mockDao(0, StreamingDao.class);
+
+        CollectionOptions collectionOptions = new CollectionOptions();
+        StreamingResource streamingResource = new StreamingResourceImpl(streamingDao, collectionOptions);
+        DisposableServer server   = testServer(streamingResource).getServer();
+        try {
+            HttpClient client = HttpClient.create().port(server.port());
+            final AtomicReference<HttpClientResponse> response = new AtomicReference<>();
+            List<String> strings = client.get().uri("/stream/paging?limit=15").response((resp, body)->{
+                response.set(resp);
+                return body.asString();
+            }).collectList().block();
+
+            assertThat(strings).hasSize(0);
+            assertThat(collectionOptions.isLastRecord()).isTrue();
+        } finally {
+            server.disposeNow();
+        }
+    }
+
     /**
      * Here the implementing class has annotated its method with stream. That should work
      */
@@ -95,6 +199,16 @@ public class StreamingDataTest {
         @Produces(MediaType.TEXT_PLAIN)
         @Path("withHeaders")
         Observable<String> streamWithHeaders();
+
+        @GET
+        @Produces(MediaType.TEXT_PLAIN)
+        @Path("paging")
+        Flux<String> streamWithPaging(@QueryParam("limit") String limit);
+    }
+
+    interface StreamingDao {
+        @Query("SELECT * FROM names WHERE name = :name")
+        Flux<String> getData(String name, CollectionOptions collectionOptions);
     }
 
     /**
@@ -110,6 +224,15 @@ public class StreamingDataTest {
     }
 
     public class StreamingResourceImpl implements StreamingResource {
+
+        private StreamingDao streamingDao;
+        private final CollectionOptions collectionOptions;
+
+        public StreamingResourceImpl(StreamingDao streamingDao, CollectionOptions collectionOptions) {
+            this.streamingDao = streamingDao;
+            this.collectionOptions = collectionOptions;
+        }
+
         @Override
         @Stream
         public Observable<String> streamOfStrings() {
@@ -127,6 +250,14 @@ public class StreamingDataTest {
             return withHeaders(just("a", "b"), new HashMap<String, String>(){{
                 put("my-header", "my-value");
             }});
+        }
+
+        @Override
+        public Flux<String> streamWithPaging(String limit) {
+            collectionOptions.setLimit(Integer.parseInt(limit));
+            return streamingDao.getData("test", collectionOptions).map(s -> {
+                return String.valueOf(s);
+            });
         }
     }
 
