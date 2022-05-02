9cf797cfc57e4841864e12fc707ee0ae83baae3e
Jonas Fredin <jonas.fredin@fortnox.se>
2022-05-02 14:06:03 +0200

    remove unused import

--- client/src/main/java/se/fortnox/reactivewizard/client/HttpClient.java
+++ client/src/main/java/se/fortnox/reactivewizard/client/HttpClient.java
@@ -40,7 +40,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
