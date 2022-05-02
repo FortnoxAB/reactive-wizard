a50c0b4b1bfc127ce7576dd0f15e04f1f372288a
Jonas Fredin <jonas.fredin@fortnox.se>
2022-05-02 14:38:11 +0200

    remove unused interface

--- client/src/main/java/se/fortnox/reactivewizard/client/RequestAuthenticator.java
+++
@@ -1,8 +0,0 @@
-package se.fortnox.reactivewizard.client;
-
-/**
- * Something that is able to make an outgoing request authenticated.
- */
-public interface RequestAuthenticator {
-    void authenticate(RequestBuilder request);
-}
