b08c9d0083f632739337ccca509b1be43db9e3fa
Tommy Persson <tommy.persson@gmail.com>
2022-12-11 16:14:09 +0100

    Make it easier to use custom classes as request parameters
    Follows the JAX-RS spec by allowing classes with:
    * A static `valueOf(String)`-method
    * A static `fromString(String)`-method
    * A `String`-constructor

---
+++ jaxrs/src/main/java/se/fortnox/reactivewizard/jaxrs/params/deserializing/CustomClassDeserializerFactory.java
@@ -0,0 +1,88 @@
+package se.fortnox.reactivewizard.jaxrs.params.deserializing;
+
+import javax.annotation.Nullable;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
+class CustomClassDeserializerFactory {
+    @Nullable
+    public static <T> Deserializer<T> createOrNull(Class<T> cls) {
+        var methods = cls.getMethods();
+
+        var valueOfDeserializer = findStaticMethodDeserializer(methods, "valueOf", cls);
+        if (valueOfDeserializer != null) {
+            return valueOfDeserializer;
+        }
+
+        var fromStringDeserializer = findStaticMethodDeserializer(methods, "fromString", cls);
+        if (fromStringDeserializer != null) {
+            return fromStringDeserializer;
+        }
+
+        var constructorDeserializer = findConstructorDeserializer(cls);
+        if (constructorDeserializer != null) {
+            return constructorDeserializer;
+        }
+
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Nullable
+    private static <T> Deserializer<T> findStaticMethodDeserializer(Method[] methods, String methodName, Class<T> cls) {
+        var method = Arrays.stream(methods)
+            .filter(it -> it.getName().equals(methodName)
+                && it.getParameterCount() == 1
+                && it.getParameterTypes()[0].equals(String.class)
+                && Modifier.isStatic(it.getModifiers())
+            )
+            .findFirst()
+            .orElse(null);
+
+        if (method != null) {
+            return value -> {
+                try {
+                    if (value == null) {
+                        return null;
+                    }
+
+                    return (T)method.invoke(null, value);
+                } catch (Exception e) {
+                    throw deserializerExceptionFor(cls);
+                }
+            };
+        }
+
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Nullable
+    private static <T> Deserializer<T> findConstructorDeserializer(Class<T> cls) {
+        var constructor = Arrays.stream(cls.getConstructors())
+            .filter(it -> it.getParameterCount() == 1 && it.getParameterTypes()[0].equals(String.class))
+            .findFirst()
+            .orElse(null);
+
+        if (constructor != null) {
+            return value -> {
+                try {
+                    if (value == null) {
+                        return null;
+                    }
+
+                    return (T)constructor.newInstance(value);
+                } catch (Exception e) {
+                    throw deserializerExceptionFor(cls);
+                }
+            };
+        }
+
+        return null;
+    }
+
+    private static DeserializerException deserializerExceptionFor(Class<?> cls)   {
+        return new DeserializerException(String.format("invalid.%s", cls.getSimpleName()));
+    }
+}
--- jaxrs/src/main/java/se/fortnox/reactivewizard/jaxrs/params/deserializing/DeserializerFactory.java
+++ jaxrs/src/main/java/se/fortnox/reactivewizard/jaxrs/params/deserializing/DeserializerFactory.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.util.StdDateFormat;
 import se.fortnox.reactivewizard.json.JsonDeserializerFactory;
 import se.fortnox.reactivewizard.util.ReflectionUtil;
 
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.ws.rs.core.MediaType;
@@ -24,13 +25,13 @@ import java.util.function.Function;
 public class DeserializerFactory {
 
     private final JsonDeserializerFactory     jsonDeserializerFactory;
-    private final Map<Class<?>, Deserializer> stringDeserializers;
+    private final Map<Class<?>, Deserializer<?>> stringDeserializers;
 
     @Inject
     public DeserializerFactory(Provider<DateFormat> dateFormatProvider, JsonDeserializerFactory jsonDeserializerFactory) {
         this.jsonDeserializerFactory = jsonDeserializerFactory;
 
-        stringDeserializers = new HashMap<Class<?>, Deserializer>() {
+        stringDeserializers = new HashMap<>() {
             {
                 put(Boolean.class, new BooleanDeserializer());
                 put(boolean.class, new BooleanNotNullDeserializer());
@@ -50,55 +51,77 @@ public class DeserializerFactory {
     }
 
     public DeserializerFactory() {
-        this(() -> new StdDateFormat(), new JsonDeserializerFactory());
+        this(StdDateFormat::new, new JsonDeserializerFactory());
     }
 
     /**
      * Return Deserializer from param type.
+     *
      * @param paramType the param type
-     * @param <T> the type of the deserializer
+     * @param <T>       the type of the deserializer
      * @return the deserializer
      */
+    @SuppressWarnings({"unchecked", "rawtypes"})
     public <T> Deserializer<T> getParamDeserializer(TypeReference<T> paramType) {
-        Class<?>        paramCls     = ReflectionUtil.getRawType(paramType.getType());
-        Deserializer<T> deserializer = (Deserializer<T>)stringDeserializers.get(paramCls);
-
-        if (deserializer != null) {
-            return deserializer;
-        }
-
-        if (paramCls.isEnum()) {
-            return new EnumDeserializer(paramCls);
-        }
+        var paramCls = ReflectionUtil.getRawType(paramType.getType());
 
         if (List.class.isAssignableFrom(paramCls)) {
-            Class<?>     elementType = ReflectionUtil.getGenericParameter(paramType.getType());
+            var          elementCls = ReflectionUtil.getGenericParameter(paramType.getType());
             Deserializer elementDeserializer;
 
-            if (elementType.isEnum()) {
-                elementDeserializer = new EnumDeserializer(elementType);
+            if (elementCls.isEnum()) {
+                elementDeserializer = new EnumDeserializer(elementCls);
             } else {
-                elementDeserializer = stringDeserializers.get(elementType);
+                elementDeserializer = getClassDeserializer(elementCls);
             }
 
             return new ListDeserializer(elementDeserializer);
         }
 
         if (paramCls.isArray()) {
-            final Deserializer elementDeserializer = stringDeserializers.get(paramCls.getComponentType());
+            final Deserializer elementDeserializer = getClassDeserializer(paramCls.getComponentType());
             return new ArrayDeserializer(elementDeserializer, paramCls.getComponentType());
+        }
+
+        var deserializer = (Deserializer<T>)getClassDeserializer(paramCls);
+        if (deserializer != null) {
+            return deserializer;
         }
 
         throw new RuntimeException("Field of type " + paramType.getType() + " is not allowed to be used in query/form/header");
     }
 
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    @Nullable
+    public <T> Deserializer<T> getClassDeserializer(Class<T> paramCls) {
+        var existingDeserializer = (Deserializer<T>)stringDeserializers.get(paramCls);
+        if (existingDeserializer != null) {
+            return existingDeserializer;
+        }
+
+        if (paramCls.isEnum()) {
+            return new EnumDeserializer(paramCls);
+        }
+
+        var customClassDeserializer = (Deserializer<T>)CustomClassDeserializerFactory.createOrNull(paramCls);
+        if (customClassDeserializer != null) {
+            stringDeserializers.put(paramCls, customClassDeserializer);
+
+            return customClassDeserializer;
+        }
+
+        return null;
+    }
+
     /**
      * Return the body deserializer for param type.
+     *
      * @param paramType the param type
-     * @param consumes the consumes requirements
-     * @param <T> type of deserializer
+     * @param consumes  the consumes requirements
+     * @param <T>       type of deserializer
      * @return the deserializer
      */
+    @SuppressWarnings("unchecked")
     public <T> BodyDeserializer<T> getBodyDeserializer(TypeReference<T> paramType, String[] consumes) {
         // Only support a single consumes for now
         String consume = consumes[0];
