3d1dc5dcf5548623a4fb062a30b427515edcd117
Rickard Blomkvist <rickard.blomkvist@fortnox.se>
2023-03-31 13:23:17 +0200

    MockConfigFactory now creates a lenient mock in order to work with TestInjector under strict settings.

--- config/src/test/java/se/fortnox/reactivewizard/config/MockConfigFactory.java
+++ config/src/test/java/se/fortnox/reactivewizard/config/MockConfigFactory.java
@@ -3,6 +3,8 @@ package se.fortnox.reactivewizard.config;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
+import static org.mockito.quality.Strictness.LENIENT;
 
 public class MockConfigFactory {
 
@@ -10,7 +12,7 @@ public class MockConfigFactory {
     }
 
     public static ConfigFactory create() {
-        ConfigFactory configFactory = mock(ConfigFactory.class);
+        ConfigFactory configFactory = mock(ConfigFactory.class, withSettings().strictness(LENIENT));
         when(configFactory.get(any())).thenAnswer(call -> call.getArgument(0, Class.class).newInstance());
         return configFactory;
     }
